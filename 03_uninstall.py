4d8317e8f1112d3c8d7271cd45076b14679089c3
Rainy <238183989+rainy-me@users.noreply.github.com>
2026-05-10 04:23:46 +0900

    Update 03_uninstall.py

--- 03_uninstall.py
+++ 03_uninstall.py
@@ -22,6 +22,9 @@ def main():
     # cfg 폴더 내 sourcemod 폴더 삭제
     shutil.rmtree(l4d2_dir / "left4dead2/cfg/sourcemod", ignore_errors=True)
 
+    # ems 폴더 삭제
+    shutil.rmtree(l4d2_dir / "left4dead2/ems", ignore_errors=True)
+
     print(f"'{l4d2_dir}'에서 삭제 완료.")
 
 
