0bf74b5223287169d06922b5dfcb9dac34440198
Rainy <238183989+rainy-me@users.noreply.github.com>
2026-08-31 22:55:02 +0900

    Update reinstall.cmd

--- reinstall.cmd
+++ reinstall.cmd
@@ -1,4 +1,3 @@
-xcopy /e /y "D:\SteamLibrary\steamapps\common\Left 4 Dead 2\left4dead2\addons\sourcemod\data\sqlite" "D:\repository\l4d2-settings\data\sqlite"
 uv run 03_uninstall.py
 uv run 01_copy.py
 uv run 02_compile.py
